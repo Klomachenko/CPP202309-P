49e9cfa8b34de027edc4d4bc2c17b2a9a78d30db
Klomachenko <leekyumin0901@naver.com>
2023-09-06 23:38:06 +0900

    Feat : hello.cpp 구현

---
+++ Reports/hello.cpp
@@ -0,0 +1,9 @@
+// 첫 번째 예제 프로그램
+#include <iostream>  // 표준 라이브러리 헤더 파일을 포함
+using namespace std; // 표준 네임스페이스를 사용
+
+int main()
+{
+    cout << "Hello World!" << endl; // 화면에 "Hello World!"를 출력
+    return 0;                       // 프로그램 종료 코드를 반환
+}
